8bedc09f6c6a68043eef9abea00910e8063ef024
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-02-21 19:49:07 -0600

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
 web: gunicorn wsgi: app
+
+const PORT = process.env.PORT || '8080'
+app=express();
+app.set("port", PORT);
--- query.sql
+++ query.sql
@@ -19,3 +19,26 @@ CREATE VIEW tested AS
 SELECT state, c.tested, c.population
 FROM covid AS c; 
 select * from flu;
+
+SELECT * FROM covid;
+
+SELECT SUM(deaths)
+FROM covid;
+
+
+
+SELECT * FROM state_data;
+
+SELECT  tested, population, 
+       (tested/population * 100.0) AS Percent
+FROM covid;
+SELECT * FROM covid;
+
+ALTER TABLE covid
+ADD (Cast(tested AS FLOAT)/(Cast(population AS Float))*100) AS Percent:
+FROM covid;
+
+CREATE VIEW percent6 AS
+SELECT state,  ROUND(Cast(infected AS FLOAT)/(Cast(tested AS Float))*100) AS Percentinfected, ROUND(Cast(tested AS FLOAT)/(Cast(population AS Float))*100) AS Percenttested
+FROM covid;
+SELECT * FROM  percent6; percent;
