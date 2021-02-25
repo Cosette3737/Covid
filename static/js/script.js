984b5bd76e4473d25670fe865a4cbd8422a364ae
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-02-24 23:51:30 -0600

    .

--- static/js/script.js
+++ static/js/script.js
@@ -30,7 +30,7 @@ function createMap (mapData) {
         var options = {
             colorAxis: {colors: ['#1c92e7','#49a7eb', '#60b2ee','#76bdf0','#8dc8f3','#a4d3f5','#badef7','#d1e9fa']},
             backgroundColor: 'eff3f6',
-            region: 'us_metro',
+            region: 'US',
             displayMode: 'regions'
         };
 
