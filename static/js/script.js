068895cfeaf00a19ee14df7de7996035f0199ebf
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-03-10 23:54:45 -0600

    .

--- static/js/script.js
+++ static/js/script.js
@@ -14,11 +14,7 @@ function createMap (mapData) {
     //pgconsole.log(mapData);
     google.charts.load('current', {
         'packages':['geochart'],
-<<<<<<< HEAD
-        'mapsApiKey': 'AIzaSyC8ZuVwhL5UUOmCw000vMesIrfUCdrcjuY'
-=======
-        'mapsApiKey':"AIzaSyCyoQn8h1sxhW6eEPE7mwh2RTWG29qOmfA"
->>>>>>> a8d0473a0333ea52285723a85d5b583ad9ebf6ce
+        'mapsApiKey':'AIzaSyCyoQn8h1sxhW6eEPE7mwh2RTWG29qOmfA'
     });
 
     // call function to draw the map
